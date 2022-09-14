bf8bbad6d97677e2004019bf6cb9f444030e0ed0
sunxyw <git@sunxyw.xyz>
2022-09-14 16:32:23 +0800

    enhance signup flow

--- app/Http/Controllers/Central/SignUpController.php
+++ app/Http/Controllers/Central/SignUpController.php
@@ -3,83 +3,110 @@
 namespace App\Http\Controllers\Central;
 
 use App\Http\Controllers\Controller;
-use App\Mail\RegisterRequested;
+use App\Models\CentralUser;
+use App\Models\Tenant;
 use App\Models\User;
 use App\Notifications\VerifyCodeRequested;
+use App\Services\FlowControlService;
 use App\Services\VerifyCodeService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 use Illuminate\Validation\Rules;
 
 class SignUpController extends Controller
 {
-    public function create()
+    private const FLOW_NAME = 'signup';
+    private const STEP_REQUEST_VERIFY_CODE = 0;
+    private const STEP_FILL_FORM = 1;
+
+    public function __construct(
+        private VerifyCodeService  $verifyCodeService,
+        private FlowControlService $flowControlService,
+    )
     {
-        return Inertia::render('@frontend::SignUp', [
-            'emailLocked' => false,
-            'email' => '',
-        ]);
+    }
+
+    private function render($data)
+    {
+        return Inertia::render('@frontend::SignUp', $data);
+    }
+
+    public function create(Request $request)
+    {
+        $key = $request->session()->get('flow_key');
+        if ($request->query('reset')) {
+            $this->flowControlService->endFlow(self::FLOW_NAME, $key);
+        }
+        if (!$this->flowControlService->isFlowStarted(self::FLOW_NAME, $key)) {
+            $key = $this->flowControlService->startFlow(self::FLOW_NAME);
+            $request->session()->put('flow_key', $key);
+        }
+        $step = $this->flowControlService->getStep(self::FLOW_NAME, $key);
+        $meta = $this->flowControlService->getMetaForFlow(self::FLOW_NAME, $key);
+        return $this->render($meta + compact('step'));
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'email' => ['required', 'email', 'unique:users,email', 'max:255', 'string'],
+            'email' => ['required', 'email', 'unique:central_users,email', 'max:255', 'string'],
             'email_verify_code' => ['present', 'nullable', 'string', 'size:6'],
             'id' => ['required_with:email_verify_code', 'nullable', 'string', 'min:6', 'max:12'],
             'name' => ['required_with:email_verify_code', 'nullable', 'string', 'max:255'],
             'password' => ['required_with:email_verify_code', 'nullable', 'string', 'min:8', 'max:255', Rules\Password::defaults()],
         ]);
 
-        if (!$request->filled('email_verify_code')) {
-            $tmp_user = new User([
-                'name' => 'Guest',
-                'email' => $request->input('email'),
-            ]);
-            $code = app(VerifyCodeService::class)->generateAndStore('email', $tmp_user->email);
-            $tmp_user->notify(new VerifyCodeRequested($code));
+        $flow_key = $request->session()->get('flow_key');
+        $step = $this->flowControlService->getStep(self::FLOW_NAME, $flow_key);
+        switch ($step) {
+            case self::STEP_REQUEST_VERIFY_CODE:
+                $this->flowControlService->storeMetaForFlow(self::FLOW_NAME, $flow_key, [
+                    'email' => $request->input('email'),
+                ]);
+                $this->flowControlService->nextStep(self::FLOW_NAME, $flow_key);
+                $tmp_user = new User([
+                    'name' => 'Guest',
+                    'email' => $request->input('email'),
+                ]);
+                $code = $this->verifyCodeService->generateAndStore('email', $tmp_user->email);
+                $tmp_user->notify(new VerifyCodeRequested($code));
+                return $this->render([
+                    'step' => self::STEP_FILL_FORM,
+                    'email' => $request->input('email'),
+                ]);
+            case self::STEP_FILL_FORM:
+                $code = $request->post('email_verify_code');
+                $id = $request->post('id');
+                $name = $request->post('name');
+                $password = $request->post('password');
 
-            return Inertia::render('@frontend::SignUp', [
-                'emailLocked' => true,
-                'email' => $request->post('email'),
-            ]);
+                if (!$this->verifyCodeService->verify('email', $request->post('email'), $code)) {
+                    return back()->withErrors([
+                        'email_verify_code' => __('The code is incorrect.'),
+                    ]);
+                }
+
+                /** @var User $c_user */
+                $c_user = CentralUser::query()->create([
+                    'name' => $name,
+                    'email' => $request->post('email'),
+                    'password' => Hash::make($password),
+                ]);
+                $tenant = Tenant::query()->create([
+                    'id' => $id,
+                ]);
+                $tenant->run(function () use ($c_user) {
+                    User::create([
+                        'global_id' => $c_user->global_id,
+                        'name' => $c_user->name,
+                        'email' => $c_user->email,
+                        'password' => $c_user->password,
+                    ]);
+                });
+                $this->flowControlService->endFlow(self::FLOW_NAME, $flow_key);
         }
 
-        $code = $request->post('email_verify_code');
-        $id = $request->post('id');
-        $name = $request->post('name');
-        $password = $request->post('password');
-
-        if (!app(VerifyCodeService::class)->verify('email', $request->post('email'), $code)) {
-            return back()->withErrors([
-                'email_verify_code' => __('The code is incorrect.'),
-            ]);
-        }
-
-        DB::beginTransaction();
-        try {
-            $user = User::create([
-                'name' => $name,
-                'email' => $request->post('email'),
-                'password' => Hash::make($password),
-            ]);
-
-            $tenant = $user->tenants()->create([
-                'id' => $id,
-            ]);
-
-            $tenant->domains()->create([
-                'domain' => $id,
-            ]);
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
-
-        return redirect()->route('home');
+        return redirect()->route('central::home');
     }
 }
